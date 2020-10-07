2198e26ced106f0545b8343c0c8b358bf5643429
Mani <maani.cr7@gmail.com>
2020-10-07 20:32:22 +1000

    profile

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { useContent } from "../hooks";
-import selectionFilter from "../utils/selection-filter";
+import { BrowseContainer } from '../containers/browse';
+import { useContent } from '../hooks';
+import {selectionFilter} from '../utils/selection-filter'
 
-export default function Browse( props ) {
-    const { series } = useContent( 'series' )
-    const { films } = useContent( 'films' )
-    const slides = selectionFilter( { series, films });
+export default function Browse() {
+    const { series } = useContent( 'series' );
+    const { films } = useContent( 'films' );
+    const slides = selectionFilter( { series, films } );
 
-    // console.log(slides)
-    return (
-        <div>
-            Hello from Browse
-        </div>
-    );
+    return <BrowseContainer slides={ slides }/>;
 }
